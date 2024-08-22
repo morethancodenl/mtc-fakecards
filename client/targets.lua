6668756ef91134ddbbac478b327b8abd0fa63eec
Timothy_ <73821729+Timothy-Dylan@users.noreply.github.com>
2024-08-22 19:44:28 +0200

    feat! Multiple Spawn Locations

--- client/targets.lua
+++ client/targets.lua
@@ -3,49 +3,51 @@ local spawnedPed = nil
 local Config = require 'config.config'
 
 -- Creates a new point
-local point = lib.points.new({coords = Config.Ped.coords.xyz, distance = 15})
+for _ , coords in pairs(Config.Ped.coords) do
+    local point = lib.points.new({coords = coords.xyz, distance = 15})
 
---- On the player entering the point it will create the eped
-function point:onEnter()
-    -- Checks if the ped is already spawned
-    if spawnedPed then return end
-    local pedHash = joaat(Config.Ped.model)
+    --- On the player entering the point it will create the eped
+    function point:onEnter()
+        -- Checks if the ped is already spawned
+        if spawnedPed then return end
+        local pedHash = joaat(Config.Ped.model)
 
-    -- Requests the model
-    lib.requestModel(pedHash)
+        -- Requests the model
+        lib.requestModel(pedHash)
 
-    -- Creates the ped
-    spawnedPed = CreatePed(4, pedHash, Config.Ped.coords, 0.0, false, false)
-    SetBlockingOfNonTemporaryEvents(spawnedPed, true)
-    SetPedDiesWhenInjured(spawnedPed, false)
-    SetPedCanPlayAmbientAnims(spawnedPed, true)
-    SetPedCanRagdollFromPlayerImpact(spawnedPed, false)
-    SetEntityInvincible(spawnedPed, true)
-    FreezeEntityPosition(spawnedPed, true)
-    SetEntityHeading(spawnedPed, Config.Ped.coords.w)
+        -- Creates the ped
+        spawnedPed = CreatePed(4, pedHash, coords, 0.0, false, false)
+        SetBlockingOfNonTemporaryEvents(spawnedPed, true)
+        SetPedDiesWhenInjured(spawnedPed, false)
+        SetPedCanPlayAmbientAnims(spawnedPed, true)
+        SetPedCanRagdollFromPlayerImpact(spawnedPed, false)
+        SetEntityInvincible(spawnedPed, true)
+        FreezeEntityPosition(spawnedPed, true)
+        SetEntityHeading(spawnedPed, coords.w)
 
-    -- Creates a new target point on the ped
-    exports.ox_target:addLocalEntity(spawnedPed, {
-        {
-            label = locale('talk_to_ped'),
-            icon = 'fas fa-id-card',
-            distance = 1.5,
-            event = 'mtc-fakecards:client:openMenu',
-            canInteract = function ()
-                -- Checks if the Player can interact if they are not a LEO
-                local player = QBX.PlayerData
-                if player.job.type == 'leo' then return false end
-                return true
-            end
-        }
-    })
+        -- Creates a new target point on the ped
+        exports.ox_target:addLocalEntity(spawnedPed, {
+            {
+                label = locale('talk_to_ped'),
+                icon = 'fas fa-id-card',
+                distance = 1.5,
+                event = 'mtc-fakecards:client:openMenu',
+                canInteract = function ()
+                    -- Checks if the Player can interact if they are not a LEO
+                    local player = QBX.PlayerData
+                    if player.job.type == 'leo' then return false end
+                    return true
+                end
+            }
+        })
 
-    SetModelAsNoLongerNeeded(pedHash)
-end
+        SetModelAsNoLongerNeeded(pedHash)
+    end
 
---- On the player exiting the point it will delete the pedId
-function point:onExit()
-    if not spawnedPed then return end
-    DeleteEntity(spawnedPed)
-    spawnedPed = nil
+    --- On the player exiting the point it will delete the pedId
+    function point:onExit()
+        if not spawnedPed then return end
+        DeleteEntity(spawnedPed)
+        spawnedPed = nil
+    end
 end
